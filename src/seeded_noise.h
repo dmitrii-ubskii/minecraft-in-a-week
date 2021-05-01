00a2cf9ab1c7ebca1525a73e9c3ef2a8c8a8c8f5
Dmitrii Ubskii <ubskydm@gmail.com>
2021-05-02 00:45:40 +0300

    Move seed plane with scale (remove lines of pillars)

--- src/seeded_noise.h
+++ src/seeded_noise.h
@@ -14,7 +14,7 @@ public:
 	{
 		auto altitude = 0.f;
 		for (auto scale = 1.f; scale < 64.f; scale *= 2.f)
-			altitude += (1 + glm::simplex(glm::vec3{seed, 0.12f + v.x / scale, 0.45f + v.y / scale})) * scale;
+			altitude += (1 + glm::simplex(glm::vec3{seed / scale, 0.12f + v.x / scale, 0.45f + v.y / scale})) * scale;
 		return (int)altitude;
 	}
 
